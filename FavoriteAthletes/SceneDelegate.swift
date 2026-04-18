b774b36f7e0e321a6563c4215a47b09faaec8f9e
Uriel Alejandro hdez <ua-x99@hotmail.com>
2026-04-18 02:25:12 -0600

    Fix: window background color to remove black cutoff areas

--- FavoriteAthletes/SceneDelegate.swift
+++ FavoriteAthletes/SceneDelegate.swift
@@ -7,7 +7,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
+        window.backgroundColor = .systemGroupedBackground
         let nav = UINavigationController(rootViewController: AthleteTableViewController(style: .plain))
+        nav.view.backgroundColor = .systemGroupedBackground
         window.rootViewController = nav
         self.window = window
         window.makeKeyAndVisible()
